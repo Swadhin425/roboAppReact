467503ef89b5f89c357c7820f730b97c6385a865
Ranjan Nayak <swadhin.ranjannayak@siemens.com>
2020-09-05 20:28:17 +0530

    Updates

---
+++ precache-manifest.e3cf458dd7fd6d86cdce8df9704ff36e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38226b3592b98a7280ea5fee321d6b06",
+    "url": "/roboAppReact/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboAppReact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aa1d99579d381060dcae",
+    "url": "/roboAppReact/static/css/main.0ddb6699.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboAppReact/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboAppReact/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1d99579d381060dcae",
+    "url": "/roboAppReact/static/js/main.575fe5ea.chunk.js"
+  },
+  {
+    "revision": "babd744122050280e0c6",
+    "url": "/roboAppReact/static/js/runtime-main.fdb2b136.js"
+  }
+]);
